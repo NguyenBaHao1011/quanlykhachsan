7c158f90a6624ef2c642c18abb27e67d0d1a52e3
TranNgocHa <tngocha130404@gmail.com>
2026-03-09 14:23:39 +0700

    Update files and folders on main branch

--- quanlykhachsan-main/git-github-flow/QLHD/HoaDon.js
+++
@@ -1,9 +0,0 @@
-function thanhToan(){
-
-let tong = document.getElementById("tong").innerText;
-
-alert("Thanh toán thành công!\nTổng tiền: " + tong + " VNĐ");
-
-window.location.href="trangchu.html";
-
-}
